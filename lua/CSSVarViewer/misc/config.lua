3e78591a0a4fb0de02f8ddfdcc8963f5f3f8a752
Hecdin <Hecdinnicola@outlook.com>
2024-05-04 18:29:42 -0400

    0.1.1 (feat): + Display multiple virtual texts on the same line when necessary
    * Display multiple virtual texts.
    * Add code comments.

--- lua/CSSVarViewer/misc/config.lua
+++ lua/CSSVarViewer/misc/config.lua
@@ -4,15 +4,12 @@ local M = {}
 M.options = {
   -- <number> Parent search limit (number of levels to search upwards)
   parent_search_limit = 5,
-  -- <string> Name of the file to track (e.g. "main" for main.lua)
+  -- <string> Name of the file to track (e.g. "main" for main.css)
   filename_to_track = "main",
-  -- <string> Pattern to search for variables containing "color"
-  variable_pattern = "%-%-[-_%w]*color[-_%w]*",
   -- <boolean> Indicates whether keymaps are disabled
   disable_keymaps = false,
-  -- <table>
+  -- <table> Filetypes to support
   filetypes = {"css", "scss", "sass"},
 }
 
 return M
-
